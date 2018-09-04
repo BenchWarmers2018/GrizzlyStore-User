56ab4fe4a73ee61aedceaeee94e02cd1185c184b
Singh <723304@1000810001142M.local>
2018-09-04 11:31:31 +1000

    First_user_branch_commit

---
+++ src/main/java/com/benchwarmers/grads/grizzlystoreuser/GrizzlystoreUserApplication.java
@@ -0,0 +1,12 @@
+package com.benchwarmers.grads.grizzlystoreuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrizzlystoreUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrizzlystoreUserApplication.class, args);
+    }
+}
