99af4012948693e432fde053cf33017b190fd212
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-27 11:28:59 +1000

    Fixed syntax error

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 servicename = grizzlystore-user
 
-echo 'Deployment in process'
+echo 'Deployment in progress'
 pwd && cd target
-sudo systemctl stop $servicename || true
-if [ -f $servicename] ; then
-	sudo rm /etc/init.d/$servicename || true
+sudo systemctl stop $servicename.service || true
+
+if [ -f $servicename ] ; then
+    sudo rm /etc/init.d/$servicename || true
+fi
+
 sudo ln -s *.jar /etc/init.d/$servicename
-sudo systemctl start $servicename
+sudo systemctl start $servicename.service
