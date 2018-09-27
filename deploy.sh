c661cc24c1c2996f52d6d4f4ff7a3f4d890b4744
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-27 11:43:08 +1000

    More problems

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 echo 'Deployment in progress'
 pwd && cd target
 
-if (( $(ps -ef | grep -v grep | grep grizzlystore-user | wc -l) > 0)) then
-	echo "User service is running...attempting to stop service!"
-	sudo systemctl stop grizzlystore-user.service || true
+if (( $(ps -ef | grep -v grep | grep grizzlystore-user | wc -l) > 0)) 
+	then
+		echo "User service is running...attempting to stop service!"
+		sudo systemctl stop grizzlystore-user.service || true
 fi
 
 if [ -f grizzlystore-user ] ; then
