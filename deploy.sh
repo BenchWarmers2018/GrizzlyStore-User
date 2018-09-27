258ac07e0f6a4ecc479464a229826ee2619c531b
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-27 11:22:15 +1000

    Edited deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+servicename = grizzlystore-user
 
 echo 'Deployment in process'
 pwd && cd target
-sudo systemctl stop grizzlystore-user.service || true
-sudo rm /etc/init.d/grizzlystore-user || true
-sudo ln -s *.jar /etc/init.d/grizzlystore-user
-sudo systemctl start grizzlystore-user.service
+sudo systemctl stop $servicename || true
+if [ -f $servicename] ; then
+	sudo rm /etc/init.d/$servicename || true
+sudo ln -s *.jar /etc/init.d/$servicename
+sudo systemctl start $servicename
